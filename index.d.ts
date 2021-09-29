5adfab81ea1fc6a0f8ab51c102fede325ad46589
Nicolas Dao <nic@neap.co>
2021-09-29 13:00:47 +1000

    chore: Add new 'schemas' option in TypeScript

--- index.d.ts
+++ index.d.ts
@@ -3,6 +3,7 @@ import { IResolvers } from 'graphql-tools/dist/Interfaces';
 interface Options {
   ignore?: string | string[];
   mode?: 'js' | 'ts' | string;
+  schemas?: string[];
 }
 
 interface Result {
